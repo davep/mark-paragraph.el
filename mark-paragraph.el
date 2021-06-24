a833207fa04ef0a6f6a1dfea0738455ec6c9c3ad
Dave Pearson <dave.pearson@synpromics.com>
2021-06-24 15:55:40 +0100

    :books: Fix a typo

--- mark-paragraph.el
+++ mark-paragraph.el
@@ -22,7 +22,7 @@
 
 ;;; Commentary:
 ;;
-;; mark-paragraph.el contains a some extra commands for marking a paragraph.
+;; mark-paragraph.el contains some extra commands for marking a paragraph.
 ;; Inspired by https://twitter.com/nriley/status/1408049507775004675, I
 ;; checked Emacs and actually couldn't see a simple way of doing it out of
 ;; the box, so I quickly knocked this up for fun.
